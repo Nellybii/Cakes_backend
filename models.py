d3b8621e350b485430fa869fe288cc6f322a1ea2
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-01-18 15:36:07 +0300

    migrations

--- models.py
+++ models.py
@@ -23,24 +23,13 @@ class Customer(db.Model):
     phone=db.Column(db.Integer, nullable=False, unique=True)
     location=db.Column(db.String(25), nullable=False)
     quantity=db.Column(db.Integer, nullable=False)
-    order_id=db.Column(db.Integer, db.ForeignKey("order.id"))
-    user_id=db.Column(db.Integer, db.ForeignKey("user.id"))
+    order_id=db.Column(db.Integer, db.ForeignKey("orders.id"))
+    user_id=db.Column(db.Integer, db.ForeignKey("users.id"))
 
     #relationships
     cakes=db.Relationship('Cake', secondary="orders", back_populates='customers')
     orders=db.Relationship('Order', back_populates='customers')
 
-class Order(db.Model):
-    __tablename__='orders'
-
-    id= db.Column(db.Integer, primary_key=True)
-    customer_id=db.Column(db.Integer, db.ForeignKey("customer.id"))
-    cake_id=db.Column(db.Integer, db.ForeignKey("cake.id"))
-    ordered_at=db.Column(db.TIMESTAMP, nullable=False)
-    to_be_delivered_at=db.Column(db.TIMESTAMP, nullable=False)
-
-    cake=db.Relationship("Cake", back_populates='orders')
-    customer=db.Relationship("Customer", back_populates='orders')
 
 class Cake(db.Model):
     __tablename__= 'cakes'
@@ -48,11 +37,22 @@ class Cake(db.Model):
     cake_flavour=db.Column(db.String, nullable=False)
     size=db.Column(db.String, nullable=False)
     order_category=db.Column(db.String, nullable=False)
-    order_id=db.Column(db.Integer, db.ForeignKey("order.id"))
+    order_id=db.Column(db.Integer, db.ForeignKey("orders.id"))
 
     customers=db.Relationship('Customers', secondary="orders", back_populates='cakes')
     orders=db.Relationship('Order', back_populates='cakes')
 
+class Order(db.Model):
+    __tablename__='orders'
+
+    id= db.Column(db.Integer, primary_key=True)
+    customer_id=db.Column(db.Integer, db.ForeignKey("customers.id"))
+    cake_id=db.Column(db.Integer, db.ForeignKey("cakes.id"))
+    ordered_at=db.Column(db.TIMESTAMP, nullable=False)
+    to_be_delivered_at=db.Column(db.TIMESTAMP, nullable=False)
+
+    cake=db.Relationship("Cake", back_populates='orders')
+    customer=db.Relationship("Customer", back_populates='orders')
   
 
 
