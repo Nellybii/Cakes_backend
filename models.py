6ae8363c45a8421f1be2d4865806470014090cfc
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-01-23 23:24:43 +0300

    models and migrations

--- models.py
+++ models.py
@@ -8,55 +8,138 @@ class User(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(25), nullable=False)
     email=db.Column(db.String(25), nullable=False, unique=True)
-    phone=db.Column(db.Integer, nullable=False, unique=True)
-    profile_picture=db.Column(db.VARCHAR, nullable=False)
-    password= db.Column(db.String(25), nullable=False)
+    phone=db.Column(db.String(25), nullable=False, unique=True)
+    password= db.Column(db.String(100), nullable=False)
+
+    # relationships
+    addresses = db.relationship('Address', backref='users', lazy=True)
+    orders=db.relationship("ShopOrder", backref="users", lazy=True)
+
+    #timestamp
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), nullable=False)
+
+
 
     def __repr__(self):
-        return f"<User{self.username}, {self.email}>"
+        return f"<User{self.username}, {self.email}>, {self.phone}"
+    
 
-class Customer(db.Model):
-    __tablename__='customers'
+# A user can have many addresses
+# An address can have many users
+class Address(db.Model):
+    __tablename__ ="addresses"
+    id= db.Column(db.Integer, primary_key=True)
+    Street_name=db.Column(db.String(25), nullable=False)
+    Street_number=db.Column(db.Integer, nullable=False)
+    region= db.Column(db.String(25), nullable=False)
+    county= db.Column(db.String(25), nullable=False)
+    # relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+        nullable=False)
+    
+    # relationships
+    users = db.relationship('User', backref='addresses', lazy=True)
+    
 
+    #Multiple users can have the same address
+
+
+    def __repr__(self):
+        return f"{self.Street_name}, {self.Street_number}>, {self.region},{self.county}>"
+    
+
+# Products
+class Product(db.Model):
+    __tablename__ ="products"
     id= db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(25), nullable=False)
-    phone=db.Column(db.Integer, nullable=False, unique=True)
-    location=db.Column(db.String(25), nullable=False)
-    quantity=db.Column(db.Integer, nullable=False)
-    order_id=db.Column(db.Integer, db.ForeignKey("orders.id"))
-    user_id=db.Column(db.Integer, db.ForeignKey("users.id"))
+    description=db.Column(db.Text, nullable=False)
+    category_id=db.Column(db.Integer, db.ForeignKey('product_categories.id'),
+        nullable=False)
+    image=db.Column(db.String(25), nullable=False)
+    price=db.Column(db.Integer, nullable=False)
 
-    #relationships
-    cakes=db.Relationship('Cake', secondary="orders", back_populates='customers')
-    orders=db.Relationship('Order', back_populates='customers')
+    users= db.relationship('User', backref='products', lazy=True)
+    promotion=db.relationship("Promotion", secondary="product_categories", back_populates="products")
 
+    def __repr__(self):
+        return f"{self.name}, {self.description}>, {self.price}"
 
-class Cake(db.Model):
-    __tablename__= 'cakes'
+# Product Category
+class ProductCategory(db.Model):
+    __tablename__ ="product_categories"
     id= db.Column(db.Integer, primary_key=True)
-    cake_flavour=db.Column(db.String, nullable=False)
-    size=db.Column(db.String, nullable=False)
-    order_category=db.Column(db.String, nullable=False)
-    order_id=db.Column(db.Integer, db.ForeignKey("orders.id"))
+    category_name=db.Column(db.String(25), nullable=False)
 
-    customers=db.Relationship('Customers', secondary="orders", back_populates='cakes')
-    orders=db.Relationship('Order', back_populates='cakes')
+    products=db.relationship('Product', backref='product_categories', lazy=True)
+    
 
-class Order(db.Model):
+
+    def __repr__(self):
+        return f"{self.category_name}"
+
+#promotion- a promotion can have many categories
+# a category can have many promotions
+class Promotion(db.Model):
+    __tablename__ ="promotions"
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(25), nullable=False)
+    description=db.Column(db.Text, nullable=False)
+    discount_rate=db.Column(db.Integer, nullable=False)
+    start_date=db.Column(db.TIMESTAMP, nullable=False)
+    end_date=db.Column(db.TIMESTAMP, nullable=False)
+
+    Productcategories= db.relationship('ProductCategory', backref='promotions', lazy=True)
+
+
+    def __repr__(self):
+        return f"{self.name}, {self.description}>, {self.discount_rate}"
+    
+
+# shopping cart
+class ShoppingCart(db.Model):
+    __tablename__ ="shopping_carts"
+    id= db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey('users.id'),
+        nullable=False)
+    
+    shopping_cart_items=db.relationship('ShoppingCartItem', backref='shopping_carts', lazy=True)
+
+    def __repr__(self):
+        return f"{self.users_id}"
+
+# Shopping cart not saved unless logged in
+class ShoppingCartItem(db.Model):
+    __tablename__ ="shopping_cart_items"
+    id= db.Column(db.Integer, primary_key=True)
+    cart_id= db.Column(db.Integer, db.ForeignKey('shopping_carts.id'),
+        nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey('users.id'),
+        nullable=False)
+    product_id= db.Column(db.Integer, db.ForeignKey('products.id'),
+        nullable=False)
+    quantity=db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"{self.user_id}, {self.quantity}"
+  
+class ShopOrder(db.Model):
     __tablename__='orders'
 
     id= db.Column(db.Integer, primary_key=True)
-    customer_id=db.Column(db.Integer, db.ForeignKey("customers.id"))
-    cake_id=db.Column(db.Integer, db.ForeignKey("cakes.id"))
-    ordered_at=db.Column(db.TIMESTAMP, nullable=False)
+    ordered_date=db.Column(db.TIMESTAMP, nullable=False)
     to_be_delivered_at=db.Column(db.TIMESTAMP, nullable=False)
+    order_total = db.Column(db.Integer, nullable=False)
+    order_status=db.Column(db.String(25), nullable=False)
+    deliver_address =db.Column(db.String(25), nullable=False)
+    payment_method =  db.Column(db.String(25), nullable=False)
+    
+    products=db.relationship("Product", back_populates="orders", lazy=True)
 
-    cake=db.Relationship("Cake", back_populates='orders')
-    customer=db.Relationship("Customer", back_populates='orders')
-  
 
 
-
+    def __repr__(self):
+       return f"<ShopOrder {self.id}, {self.ordered_date}, {self.order_status}, {self.deliver_address}>"
 
 
 
